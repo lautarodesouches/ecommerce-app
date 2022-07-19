8950b31ba88c905070053b39ca2cc26005244493
Lautaro Desouches <lautarodesouches@outlook.com>
2022-07-19 13:08:43 -0300

    Cart items

--- src/screens/CartScreen/index.js
+++ src/screens/CartScreen/index.js
@@ -1,16 +1,45 @@
-import { Text, View } from 'react-native'
+import { FlatList, Image, Text, View } from 'react-native'
 import { useSelector } from 'react-redux'
+import { PrimaryButton } from '../../components'
+import { PRODUCT_IMAGE_URL } from '../../constants/productImage'
 import { styles } from './styles'
 
 const CartScreen = () => {
 
     const cart = useSelector(state => state.product.cart)
 
-    console.log(cart)
+    const renderItem = ({ item }) => (
+        <View style={styles.item}>
+            <View style={styles.itemSection}>
+                <Image source={{ uri: `${PRODUCT_IMAGE_URL}${item.id}-1.png` }} style={styles.image} />
+            </View>
+            <View style={styles.itemSection}>
+                <Text style={styles.itemTitle}>{item.name}</Text>
+            </View>
+            <View style={styles.itemSection}>
+                <Text style={styles.itemText}>Precio por item: ${Math.round(item.pricePerItem)}</Text>
+            </View>
+            <View style={styles.itemSection}>
+                <Text style={styles.itemText}>Cantidad: {item.quantity}</Text>
+            </View>
+            <View style={styles.itemSection}>
+                <Text style={styles.itemSubTotal}>Subtotal: ${Math.round(item.quantity * item.pricePerItem)}</Text>
+            </View>
+        </View>
+    )
 
     return (
         <View style={styles.container}>
-            <Text>Cart</Text>
+            <Text style={styles.title}>Carrito:</Text>
+            <FlatList
+                data={cart}
+                renderItem={renderItem}
+                keyExtractor={item => item.id}
+            />
+            <View style={styles.totalContainer}>
+                <Text style={styles.total}>Total: ${cart.reduce((acc, item) => acc + Math.round(item.pricePerItem * item.quantity), 0)}</Text>
+                <PrimaryButton title='Checkout' />
+            </View>
         </View>
     )
 }
--- src/screens/CartScreen/styles.js
+++ src/screens/CartScreen/styles.js
@@ -1,9 +1,61 @@
 import { StyleSheet } from 'react-native'
+import { primaryBg, themeBg, themeText } from '../../constants/colors'
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: themeBg,
+    },
+    title: {
+        fontSize: 24,
+        textAlign: 'center',
+        marginVertical: 20,
+        color: themeText,
+    },
+    item: {
+        backgroundColor: 'white',
+        marginHorizontal: '5%',
+        marginVertical: 10,
+        borderRadius: 5,
+        padding: 10,
+        justifyContent: 'space-evenly',
+        alignItems: 'center'
+    },
+    itemSection: {
+        flex: 1,
+        padding: 2,
+        width: '100%',
+        marginVertical: 5,
+    },
+    image: {
+        flex: 1,
+        width: '100%',
+        height: 100,
+        resizeMode: 'contain'
+    },
+    itemTitle: {
+        fontSize: 18,
+        textAlign: 'center',
+        fontFamily: 'LatoRegular',
+    },
+    itemText: {
+        fontSize: 16,
+        textAlign: 'center',
+    },
+    itemSubTotal: {
+        fontSize: 18,
+        textAlign: 'center',
+        fontFamily: 'LatoBold',
+    },
+    totalContainer: {
+        borderColor: primaryBg,
+        borderTopWidth: 1,
+        alignSelf: 'center',
+        width: '80%'
+    },
+    total: {
+        fontSize: 20,
+        textAlign: 'center',
+        marginVertical: 5,
     }
 })
