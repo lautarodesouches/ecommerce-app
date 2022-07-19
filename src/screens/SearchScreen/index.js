aaab9433bc95a49983892f8634a161c3614a6af1
Lautaro Desouches <lautarodesouches@outlook.com>
2022-07-19 16:18:08 -0300

    Search screen

--- src/screens/SearchScreen/index.js
+++ src/screens/SearchScreen/index.js
@@ -1,14 +1,40 @@
-import { Text, View } from 'react-native'
+import { FlatList, Text, View } from 'react-native'
 import { styles } from './styles'
 import { useSelector } from 'react-redux'
+import { Header, ProductPreview, SecondaryButton } from '../../components'
+import { useEffect, useState } from 'react'
 
-const SearchScreen = () => {
+const SearchScreen = ({ navigation }) => {
 
-    const query = useSelector(state => state.search.query)
+    const query = useSelector(state => state.search.query.toLowerCase())
+    const products = useSelector(state => state.product.products)
+
+    const [filteredProducts, setFilteredProducts] = useState([])
+
+
+    useEffect(() => {
+        setFilteredProducts(products.filter(product => product.name.toLowerCase().includes(query) || product.brand.toLowerCase().includes(query) || product.category.toLowerCase().includes(query)))
+    }, [query])
+
+    const renderItem = ({ item }) => <ProductPreview handleSelected={() => navigation.navigate('ProductDetail', { itemId: item.id })} item={item} />
+
+    const ListEmptyComponent = () => (
+        <View style={styles.emptyContainer}>
+            <Text style={styles.emptyText}>No se encontraron productos que coincidan con esa búsqueda</Text>
+            <SecondaryButton title='Ver todos' />
+        </View>
+    )
 
     return (
         <View style={styles.container}>
-            <Text>SearchScreen</Text>
+            <Header navigation={navigation} />
+            <FlatList
+                extraData={filteredProducts}
+                data={filteredProducts}
+                renderItem={renderItem}
+                ListEmptyComponent={ListEmptyComponent}
+                contentContainerStyle={styles.contentContainer}
+            />
         </View>
     )
 }
--- src/screens/SearchScreen/styles.js
+++ src/screens/SearchScreen/styles.js
@@ -3,7 +3,20 @@ import { StyleSheet } from 'react-native'
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    emptyContainer: {
+        flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-    }
+    },
+    emptyText: {
+        fontSize: 18,
+        fontFamily: 'LatoRegular',
+        textAlign: 'center',
+        marginBottom: 20,
+    },
+    contentContainer: {
+        padding: '5%',
+        flexGrow: 1,
+    },
 })
