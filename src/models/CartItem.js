9e53a87b07ffa9c4218de09c93f9363047753637
Lautaro Desouches <lautarodesouches@outlook.com>
2022-07-28 17:10:59 -0300

    Fix model

--- src/models/CartItem.js
+++ src/models/CartItem.js
@@ -1,11 +1,11 @@
 class CartItem {
     constructor(id, name, pricePerItem, freeShipping, quantity, imageUri) {
-        this.id = id,
-            this.name = name,
-            this.pricePerItem = pricePerItem,
-            this.freeShipping = freeShipping,
-            this.quantity = quantity,
-            this.imageUri = imageUri
+        this.id = id
+        this.name = name
+        this.pricePerItem = pricePerItem
+        this.freeShipping = freeShipping
+        this.quantity = quantity
+        this.imageUri = imageUri
     }
 }
 
